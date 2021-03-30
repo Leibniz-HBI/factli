62b2e5fd1fa4f2d678ef6eabaefb22fbe1c86066
Aditya Kumar <ads1996.k@gmail.com>
2021-03-30 02:15:50 +0200

    Final_Update

--- Faceboook/get_list.py
+++ Faceboook/get_list.py
@@ -4,61 +4,86 @@ import time
 import pandas as pd
 from datetime import date
 
-
 def ct_get_lists(list_id, count, access_token):
-
+    
     all_accounts = pd.DataFrame(columns=[''])
-    query = 'https://api.crowdtangle.com/lists/' + \
-        str(list_id) + '/accounts?token=' + \
-        str(access_token) + '&count=' + str(count)
-    # print(query)
-    r = requests.get(query)
+    query = 'https://api.crowdtangle.com/lists/' + str(list_id) + '/accounts?token=' + str(access_token) + '&count=' + str(count) 
+    
+    
 
-    while(query != None):
+    while(query != ''):
+        try:
+    
+            print ("Fetching:\t", query)
+    
+            r = requests.get(query)
+            json_response = r.json()
+            #print(json_response)
+            next_Page_url = json_response['result']['pagination']['nextPage']
+            
+        
+        
+            normalized_json = pd.json_normalize(json_response['result']['accounts'])
+            #print(normalized_json)
+            data_frame = pd.DataFrame.from_dict(normalized_json, orient = "columns")
+            
+        
+            pd.set_option('display.max_columns', None)  
+            pd.set_option('display.max_rows', None)  
+            pd.set_option('display.max_colwidth', None) 
+            #print (accounts)
+            accounts_count = len(data_frame)
+            #print(accounts_count)
+            status = r.status_code
 
-        print("Fetching:\t", query)
+            if (status == 429):
 
-        r = requests.get(query)
-        normalized_json = pd.json_normalize(r.json())
-        data_frame = pd.DataFrame.from_dict(normalized_json, orient="columns")
-        # print(data_frame)
+                print("API rate limit hit, sleeping...")
+                time.sleep(60)
 
-        pd.set_option('display.max_columns', None)
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_colwidth', -1)
-        #print (accounts)
-        accounts_count = len(data_frame['result.accounts'])
-        # print(accounts_count)
-        status = r.status_code
+                continue
+            
 
-        if (status == 429):
+            elif(status == 200 and accounts_count != 0):
 
-            print("API rate limit hit, sleeping...")
-            time.sleep(60)
+                accounts = data_frame
+                all_accounts = all_accounts.append(accounts)
+                query = next_Page_url
+                
 
-            continue
-
-        elif(status == 200):
-
-            accounts = data_frame['result.accounts']
-            print(accounts)
-            all_accounts = pd.concat([all_accounts, accounts])
-
-            query = data_frame['result.pagination.nextPage']
-
-        else:
-            print("Other Status: ", status)
-            query = None
-
+            
+            else:
+                print("Other Status: ", status)
+                query = ''
+            
+        except KeyError:
+            print("No next page")
+            break
+    
     return(all_accounts)
 
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    
+    
     list_id = 1484485
     access_token = "xVwti7hHe2SYdVSGyDJ42TCSms7XvmStrWVIy41b"
     count = 1000
     accounts_df = ct_get_lists(list_id, count, access_token)
     #print(accounts_df)
-    #file_name = str(date.today()) + ".csv"
-    #accounts_df.to_csv(file_name)
+    file_name = str(date.today()) + ".csv" 
+    accounts_df.to_csv(file_name, encoding = 'utf-8')
+
+
+
+    
+    
+
+
+
+ 
+ 
+   
+
+
