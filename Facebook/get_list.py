8a8c550d9c840cfcf3b5d9a64b52c2e51a014b84
Aditya Kumar <ads1996.k@gmail.com>
2021-04-14 17:22:12 +0200

    Update

--- Facebook/get_list.py
+++ Facebook/get_list.py
@@ -1,40 +1,48 @@
 import requests
-import json
 import time
 import pandas as pd
 from datetime import date
+from loguru import logger
+
 
 def ct_get_lists(list_id, count, access_token):
-    
+    '''
+    This function generates a data frame containing all the
+    accounts (with information) for the given List ID.
+
+    Args:
+        list_id (int): This ID corresponds to the saved list of Facebook Pages
+        count (int): The number of accounts to be returned per API request.
+        access_token(str): The access token associated with your CrowdTangle account
+
+    Returns: Complete data frame containing all the accounts
+    '''
+
     all_accounts = pd.DataFrame(columns=[''])
-    query = 'https://api.crowdtangle.com/lists/' + str(list_id) + '/accounts?token=' + str(access_token) + '&count=' + str(count) 
-    
-    
+    query = 'https://api.crowdtangle.com/lists/' + \
+        str(list_id) + '/accounts?token=' + \
+        str(access_token) + '&count=' + str(count)
 
     while(query != ''):
         try:
-    
-            print ("Fetching:\t", query)
-    
+            print("Fetching:\t", query)
+
             r = requests.get(query)
             json_response = r.json()
-            #print(json_response)
+            logger.debug(json_response)
             next_Page_url = json_response['result']['pagination']['nextPage']
-            
-        
-        
-            normalized_json = pd.json_normalize(json_response['result']['accounts'])
-            #print(normalized_json)
-            data_frame = pd.DataFrame.from_dict(normalized_json, orient = "columns").astype({'id' : str})
-            
-            #data_frame['id'] = data_frame['id'].astype(str)
-            
-            pd.set_option('display.max_columns', None)  
-            pd.set_option('display.max_rows', None)  
-            pd.set_option('display.max_colwidth', None) 
-            #print (accounts)
+
+            normalized_json = pd.json_normalize(
+                json_response['result']['accounts'])
+
+            data_frame = pd.DataFrame.from_dict(
+                normalized_json, orient="columns", dtype=str)
+
+            pd.set_option('display.max_columns', None)
+            pd.set_option('display.max_rows', None)
+            pd.set_option('display.max_colwidth', None)
             accounts_count = len(data_frame)
-            #print(accounts_count)
+            logger.debug(accounts_count)
             status = r.status_code
 
             if (status == 429):
@@ -43,48 +51,31 @@ def ct_get_lists(list_id, count, access_token):
                 time.sleep(60)
 
                 continue
-            
 
             elif(status == 200 and accounts_count != 0):
 
                 accounts = data_frame
                 all_accounts = all_accounts.append(accounts)
                 query = next_Page_url
-                
 
-            
             else:
                 print("Other Status: ", status)
                 query = ''
-            
+
         except KeyError:
             print("No next page")
             break
-    
+
     return(all_accounts)
 
 
-
 if __name__ == "__main__":
-    
-    
+
     list_id = 1484485
     access_token = "xVwti7hHe2SYdVSGyDJ42TCSms7XvmStrWVIy41b"
     count = 1000
     accounts_df = ct_get_lists(list_id, count, access_token)
-    #print(accounts_df)
-    file_name = str(date.today()) + ".csv" 
-    accounts_df.to_csv(file_name, encoding = 'utf-8')
-
-
-
-    
-    
-
-
-
- 
- 
-   
-
-
+    # print(accounts_df)
+    file_name = str(date.today()) + ".csv"
+    accounts_df.to_csv(file_name, encoding='utf-8')
+    print(help(ct_get_lists))
--- Facebook/number_of_posts.py
+++ Facebook/number_of_posts.py
@@ -1,26 +1,35 @@
 import requests
-import json
 import time
 import pandas as pd
 from datetime import date
-import logging
+from loguru import logger
 
 logging.basicConfig(filename='posts.log', level=logging.DEBUG)
 
+
 def ct_get_posts(list_id, count, access_token):
-    
+    '''
+    This function generates a data frame containing all the
+    accounts (with information) for the given List ID.
+
+    Args:
+        list_id (int): This ID corresponds to the saved list of Facebook Pages
+        count (int): The number of posts to be returned per API request.
+        access_token(str): The access token associated with your CrowdTangle account
+
+    Returns: Complete data frame containing all the accounts
+    '''
     all_posts = pd.DataFrame(columns=[''])
     start_date = '2021-04-11'
     end_date = '2021-04-12'
-    query = 'https://api.crowdtangle.com/' + '/posts?token=' + str(access_token) + '&listIds=' + str(list_id)  + '&startDate=' + start_date + '&endDate=' + end_date + '&count=' + str(count) 
-    
-    
+    query = 'https://api.crowdtangle.com/' + '/posts?token=' + str(access_token) + '&listIds=' + str(
+        list_id) + '&startDate=' + start_date + '&endDate=' + end_date + '&count=' + str(count)
 
     while(query != ''):
         try:
-    
-            print ("Fetching:\t", query)
-    
+
+            print("Fetching:\t", query)
+
             r = requests.get(query)
 
             json_response = r.json()
@@ -32,18 +41,15 @@ def ct_get_posts(list_id, count, access_token):
                 continue
             logging.debug(json_response)
             next_Page_url = json_response['result']['pagination']['nextPage']
-            
-        
-        
-            normalized_json = pd.json_normalize(json_response['result']['posts'])
+
+            normalized_json = pd.json_normalize(
+                json_response['result']['posts'])
             logging.debug(normalized_json)
-            data_frame = pd.DataFrame.from_dict(normalized_json, orient = "columns").astype({'account.id': str})
-            
-            #data_frame['id'] = data_frame['id'].astype(str)
-            
-            pd.set_option('display.max_columns', None)  
-            pd.set_option('display.max_rows', None)  
-            pd.set_option('display.max_colwidth', None) 
+            data_frame = pd.DataFrame.from_dict(
+                normalized_json, orient="columns", dtype=str)
+            pd.set_option('display.max_columns', None)
+            pd.set_option('display.max_rows', None)
+            pd.set_option('display.max_colwidth', None)
 
             logging.debug(data_frame)
 
@@ -52,52 +58,36 @@ def ct_get_posts(list_id, count, access_token):
 
             status = r.status_code
             logging.debug(status)
-            
+
             if(status == 200 and posts_count != 0):
 
                 posts = data_frame
                 all_posts = all_posts.append(posts)
                 query = next_Page_url
-                
 
-            
             else:
                 print("Other Status: ", status)
                 query = ''
-            
+
         except KeyError:
             print("No next page")
             logging.debug(json_response)
             break
-    
+
     return(all_posts)
 
 
-
 if __name__ == "__main__":
-    
-    
+
     list_id = 1484485
     access_token = "xVwti7hHe2SYdVSGyDJ42TCSms7XvmStrWVIy41b"
     count = 100
     posts_df = ct_get_posts(list_id, count, access_token)
-    
+
     df = posts_df.groupby(['account.id']).count()[['id']]
-    
+
     logging.debug(df)
-    df_new = df.rename(columns = {'account.id': 'User IDs', 'id': 'Number of Posts'})
-    file_name = str(date.today()) + ".csv" 
-    df_new.to_csv(file_name, encoding = 'utf-8')
-
-
-
-    
-    
-
-
-
- 
- 
-   
-
-
+    df_new = df.rename(
+        columns={'account.id': 'User IDs', 'id': 'Number of Posts'})
+    file_name = str(date.today()) + ".csv"
+    df_new.to_csv(file_name, encoding='utf-8')
